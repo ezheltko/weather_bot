2813318d5f369bab950af6cc6fb6cf0f30b6b3c0
Eugene Zheltko <ezheltko@mail.ru>
2025-11-01 21:58:39 +0300

    init

---
+++ get_weather.py
@@ -0,0 +1,22 @@
+import requests
+
+# api: https://www.weatherapi.com/docs/
+
+city = 'Вилейка'
+api_key = '8d2d07864a5d4c67a00171059250111'
+url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no'
+
+# make request to api and get weather_data in json
+weather_data = requests.get(url).json()
+
+
+#  get the weather indicators
+location = weather_data['location']['name']
+condition = weather_data['current']['condition']['text']
+temperature = weather_data['current']['temp_c']
+humidity = weather_data['current']['humidity']
+pressure = weather_data['current']['pressure_mb']
+wind_speed = weather_data['current']['wind_kph']
+wind_direction = weather_data['current']['wind_dir']
+
+print(location, condition, temperature, humidity, pressure, wind_speed, wind_direction)
