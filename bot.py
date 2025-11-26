3ed8ab4210422c91dcaa379eab0fc1502d153349
Eugene Zheltko <zheltkoeugene@gmail.com>
2025-11-26 22:35:52 +0300

    add fsm add function for sending name of clients city manual

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
 from config import bot_token, weather_api_key
 from get_weather import get_weather_indicators
+
+
+# состояние FMS
+class WeatherStates(StatesGroup):
+    waiting_for_city = State()
+
 
 # состояние пользователей
 user_data = {}
@@ -34,10 +42,19 @@ async def process_help_command(message: Message):
     await message.answer('Я буду присылать тебе прогноз погоды')
 
 
-# обработчик кнопки ввести город
+# обработчик нажатия кнопки ввести город
 @dp.message(F.text == "Ввести город")
-async def get_location_man(message: Message):
+async def ask_city(message: Message, state: FSMContext):
     await message.answer('Введи название населённого пункта', reply_markup=ReplyKeyboardRemove())
+    await state.set_state(WeatherStates.waiting_for_city)
+
+
+# обработчик ввода города
+@dp.message(WeatherStates.waiting_for_city)
+async def get_city(message: Message, state: FSMContext):
+    user_data[message.from_user.id] = message.text.strip()
+    await message.answer(f'Температура в {get_weather_indicators(user_data[message.from_user.id], weather_api_key)}')
+    await state.clear()
 
 
 # обработчик кнопки показать погоду
@@ -51,6 +68,7 @@ async def process_send_weather(message: Message):
 async def process_location(message: Message):
     await message.answer(text=f'Ваши координаты: {message.location.latitude, message.location.longitude} ')
     user_data[message.from_user.id] = f"{message.location.latitude},%20{message.location.longitude}"
+    await message.answer(f'Температура в {get_weather_indicators(user_data[message.from_user.id], weather_api_key)}')
 
     print(user_data)
 
