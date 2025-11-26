f3b4a6f9a37d312a4e68b87ab3c888c2562e50fd
Eugene Zheltko <zheltkoeugene@gmail.com>
2025-11-26 22:11:00 +0300

    add weather_but and handler

--- bot.py
+++ bot.py
@@ -5,47 +5,48 @@ from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKey
 from config import bot_token, weather_api_key
 from get_weather import get_weather_indicators
 
-# dict for user data
+# состояние пользователей
 user_data = {}
 
-# make bot and dispatcher objects
+# объекты бота и деспетчера
 bot = Bot(token=bot_token)
 dp = Dispatcher()
 
-# create button objects
-but_1 = KeyboardButton(text="Отправить геолокацию", request_location=True)
-but_2 = KeyboardButton(text="Ввести город")
+# кнопки клавиатуры
+geo_but = KeyboardButton(text="Отправить геолокацию", request_location=True)
+city_but = KeyboardButton(text="Ввести город")
+weather_but = KeyboardButton(text="Показать погоду")
 
-# create keyboard object
-keyboard_1 = ReplyKeyboardMarkup(keyboard=[[but_1], [but_2]], resize_keyboard=True)
+# объект клавиатуры
+keyboard_1 = ReplyKeyboardMarkup(keyboard=[[geo_but], [city_but], [weather_but]], resize_keyboard=True)
 
 
-# start handler
+# обработчик команды старт
 @dp.message(Command(commands="start"))
 async def process_start_command(message: Message):
     await message.answer("Привет!\nЯ буду присылать тебе прогноз погоды\n"
                          "Укажи свой населенный пункт", reply_markup=keyboard_1)
 
 
-# help handler
+# обработчик команды помощь
 @dp.message(Command(commands="help"))
 async def process_help_command(message: Message):
     await message.answer('Я буду присылать тебе прогноз погоды')
 
 
-# send location manually handler
+# обработчик кнопки ввести город
 @dp.message(F.text == "Ввести город")
 async def get_location_man(message: Message):
-    await message.answer('Введите название вашего населённого пункта', reply_markup=ReplyKeyboardRemove())
+    await message.answer('Введи название населённого пункта', reply_markup=ReplyKeyboardRemove())
 
 
-
-@dp.message(Command(commands="pogoda"))
+# обработчик кнопки показать погоду
+@dp.message(F.text == "Показать погоду")
 async def process_send_weather(message: Message):
     await message.answer(f'Температура в {get_weather_indicators(user_data[message.from_user.id], weather_api_key)}')
 
 
-# push button "отправить геолокацию"
+# обработчик кнопки "отправить геолокацию"
 @dp.message(F.location)
 async def process_location(message: Message):
     await message.answer(text=f'Ваши координаты: {message.location.latitude, message.location.longitude} ')
