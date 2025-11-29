249c637bc469fd284bc187f95a7f8929cd4c0b55
Eugene Zheltko <zheltkoeugene@gmail.com>
2025-11-29 19:59:52 +0300

    created a handler in the absence of data on the user's city using the FMS

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command
+from aiogram.filters import Command, StateFilter
 from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.state import State, StatesGroup, default_state
 from aiogram.fsm.context import FSMContext
 
 from config import bot_token, weather_api_key
@@ -11,6 +11,7 @@ from get_weather import get_weather_indicators
 # состояние FMS
 class WeatherStates(StatesGroup):
     waiting_for_city = State()
+    having_city = State()
 
 
 # состояние пользователей
@@ -26,41 +27,48 @@ city_but = KeyboardButton(text="Ввести город")
 weather_but = KeyboardButton(text="Показать погоду")
 
 # объект клавиатуры
-keyboard_1 = ReplyKeyboardMarkup(keyboard=[[geo_but], [city_but], [weather_but]], resize_keyboard=True)
+keyboard_1 = ReplyKeyboardMarkup(keyboard=[[geo_but], [city_but]], resize_keyboard=True)
+keyboard_2 = ReplyKeyboardMarkup(keyboard=[[weather_but]])
 
 
 # обработчик команды старт
-@dp.message(Command(commands="start"))
+@dp.message(Command(commands="start"), StateFilter(default_state))
 async def process_start_command(message: Message):
     await message.answer("Привет!\nЯ буду присылать тебе прогноз погоды\n"
                          "Укажи свой населенный пункт", reply_markup=keyboard_1)
 
 
 # обработчик команды помощь
-@dp.message(Command(commands="help"))
+@dp.message(Command(commands="help"), StateFilter(default_state, WeatherStates.having_city))
 async def process_help_command(message: Message):
     await message.answer('Я буду присылать тебе прогноз погоды')
 
 
 # обработчик нажатия кнопки ввести город
-@dp.message(F.text == "Ввести город")
+@dp.message(F.text == "Ввести город", StateFilter(default_state))
 async def ask_city(message: Message, state: FSMContext):
     await message.answer('Введи название населённого пункта', reply_markup=ReplyKeyboardRemove())
     await state.set_state(WeatherStates.waiting_for_city)
 
 
 # обработчик ввода города
-@dp.message(WeatherStates.waiting_for_city)
+@dp.message(StateFilter(WeatherStates.waiting_for_city))
 async def get_city(message: Message, state: FSMContext):
     user_data[message.from_user.id] = message.text.strip()
+    await state.set_state(WeatherStates.having_city)
     await message.answer(f'Температура в {get_weather_indicators(user_data[message.from_user.id], weather_api_key)}')
-    await state.clear()
 
 
-# обработчик кнопки показать погоду
-@dp.message(F.text == "Показать погоду")
+# обработчик кнопки показать погоду, зная город пользователя
+@dp.message(Command(commands="weather"), F.text == "Показать погоду", StateFilter(WeatherStates.having_city))
 async def process_send_weather(message: Message):
     await message.answer(f'Температура в {get_weather_indicators(user_data[message.from_user.id], weather_api_key)}')
+
+
+# обработчик кнопки показать погоду, НЕ зная город пользователя
+@dp.message(Command(commands="weather"), F.text == "Показать погоду", StateFilter(default_state))
+async def process_send_weather(message: Message):
+    await message.answer('Отправте свою локацию', reply_markup=keyboard_1)
 
 
 # обработчик кнопки "отправить геолокацию"
